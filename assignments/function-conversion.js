74875fc8101e621b8096713e4f46eb047c58b7d1
Angela Flowers <me@angela.flowers>
2019-07-22 15:26:26 -0400

    function-conversion.js stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -42,3 +42,7 @@ console.log(subtract(1,2));
 //   return num * 3;
 // });
 // console.log(triple);
+
+const exampleArray = [1, 2, 3, 4]
+const triple = exampleArray.map((num) => num * 3);
+console.log(triple);

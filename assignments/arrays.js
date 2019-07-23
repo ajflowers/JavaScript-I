4c199a798f6b219f00de9d8d8d22b0194dfac5f8
Angela Flowers <me@angela.flowers>
2019-07-22 20:12:02 -0400

    fixed errors in for loops

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -73,7 +73,7 @@ console.log(inventory[lastCar].car_make, inventory[lastCar].car_model);
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-for (var i = 1; i < inventory.length; i++) {
+for (var i = 0; i < inventory.length; i++) {
     carModels.push(inventory[i].car_model);
 }
 carModels.sort();
@@ -82,7 +82,7 @@ console.log(carModels);
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-for (var i = 1; i < inventory.length; i++) {
+for (var i = 0; i < inventory.length; i++) {
     carYears.push(inventory[i].car_year);
 }
 console.log(carYears);
@@ -90,7 +90,7 @@ console.log(carYears);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-for (var i = 1; i < carYears.length; i++) {
+for (var i = 0; i < carYears.length; i++) {
     if (carYears[i] < 2000) {
         oldCars.push(carYears[i]);
     }
@@ -100,7 +100,7 @@ console.log(oldCars.length);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
-for (var i = 1; i < inventory.length; i++) {
+for (var i = 0; i < inventory.length; i++) {
     if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
         BMWAndAudi.push(inventory[i]);
     }
@@ -142,5 +142,18 @@ function funGerman(car) {
 
 let ringsRoundels = inventory.filter(funGerman);
 
-//console.log(JSON.stringify(ringsRoundels));
 console.table(ringsRoundels);
+
+//again, but via callbacks
+
+let prefMakes = ["BMW", "Audi"];
+let prefYears = [1995, 2005];
+
+function byMake(makes) {
+
+}
+
+function byYear(years) {
+
+}
+
